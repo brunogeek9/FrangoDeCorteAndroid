b9e614867319156b93bf5a4d45ce151bd8d35222
brunogeek9 <brunogeek9@gmail.com>
2017-09-24 12:10:20 -0300

    leitura das regras no jfuzzy adaptada para android

--- app/src/main/java/com/example/jamelli/frangodecorte/MainActivity.java
+++ app/src/main/java/com/example/jamelli/frangodecorte/MainActivity.java
@@ -21,41 +21,34 @@ public class MainActivity extends AppCompatActivity {
     EditText et_temperatura;
     EditText et_umidade;
     double idade = 0,temperatura = 0,umidade = 0;
-    Context context = getApplicationContext();
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bnt_salvar = (Button) findViewById(R.id.button);
-
         et_idade = (EditText) findViewById(R.id.editText);
         et_temperatura = (EditText) findViewById(R.id.editText2);
         et_umidade = (EditText) findViewById(R.id.editText3);
-        bnt_salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idade = Double.parseDouble(et_idade.getText().toString());
-                temperatura = Double.parseDouble(et_temperatura.getText().toString());
-                umidade = Double.parseDouble(et_umidade.getText().toString());
 
-                new Thread(new Runnable() {
-                    public void run() {
-                        FrangoDeCorte f = new FrangoDeCorte(idade,temperatura,umidade);
-                        FuzzyFrangoDeCorte fz = new FuzzyFrangoDeCorte();
-                        fz.setEntradas(f);
+    }
 
-                        //mostrando um toast com o resultado
-                        String text = String.valueOf("CR "+fz.getSaidas()[0] + "\nCA "+fz.getSaidas()[1] +
-                                "\nGP " + fz.getSaidas()[2]);
-                        //Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
-                        //toast.show();
-                        Log.i("fuzzy",text);
+    public void salvar(View v){
+        idade = Double.parseDouble(et_idade.getText().toString());
+        temperatura = Double.parseDouble(et_temperatura.getText().toString());
+        umidade = Double.parseDouble(et_umidade.getText().toString());
 
-                    }
-                }).start();
-            }
-        });
+        FrangoDeCorte f = new FrangoDeCorte(idade,temperatura,umidade);
+        FuzzyFrangoDeCorte fz = new FuzzyFrangoDeCorte(this);
+
+        fz.setEntradas(f);
+
+        //mostrando um toast com o resultado
+        String text = String.valueOf("CR "+fz.getSaidas()[0] + "\nCA "+fz.getSaidas()[1] +
+                "\nGP " + fz.getSaidas()[2]);
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+        toast.show();
+        //Log.i("fuzzy",text);
     }
 }
--- app/src/main/java/com/example/jamelli/frangodecorte/fuzzy/FuzzyFrangoDeCorte.java
+++ app/src/main/java/com/example/jamelli/frangodecorte/fuzzy/FuzzyFrangoDeCorte.java
@@ -1,23 +1,32 @@
 package com.example.jamelli.frangodecorte.fuzzy;
 
+import android.content.Context;
+
+import com.example.jamelli.frangodecorte.MainActivity;
+
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
+import java.io.InputStream;
+
 public class FuzzyFrangoDeCorte {
+	private Context context;
 	private FIS fis;
 	//construtor da classe que carrega as regras para o objeto fis
-	public FuzzyFrangoDeCorte(){
-		try{
-			fis = FIS.load("src/resource/regras.fcl", true);
-		}catch(Exception e){
-			System.err.println("Can't load file" + e);
-		}
-		
+	public FuzzyFrangoDeCorte(MainActivity tela){
+		context = tela;
 	}
 	
 	//altera as entradas do objeto fis
 	public void setEntradas(FrangoDeCorte fdc){
+		try{
+			InputStream inputStream = context.getAssets().open("regras.fcl");
+			FIS fis = FIS.load(inputStream, true);
+		}catch(Exception e){
+			System.err.println("Can't load file" + e);
+		}
+
 		fis.setVariable("idadeAve", fdc.getIdadeAve());
         
         fis.setVariable("temperaturaDoAr", fdc.getTemperaturaDoAr());
@@ -45,9 +54,9 @@ public class FuzzyFrangoDeCorte {
 	//retorna as saidas de maneira numerica, com [0] = cr, [1] = ca, [2] = gp
 	public Double[] getSaidas(){
 
-		Variable cr = fis.getFunctionBlock("frangoDeCorte").getVariable("CR");
-	    Variable ca = fis.getFunctionBlock("frangoDeCorte").getVariable("CA");
-	    Variable gp = fis.getFunctionBlock("frangoDeCorte").getVariable("GP");
+		Variable cr = fis.getVariable("CR");
+	    Variable ca = fis.getVariable("CA");
+	    Variable gp = fis.getVariable("GP");
 		
 		Double[] saidas = new Double[3];
 		saidas[0] = cr.getValue();
